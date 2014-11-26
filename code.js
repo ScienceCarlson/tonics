3cd4aa332c83ad76c00800fd2ee818f78a0fd2b6
Wall-Dough <awalden0808@gmail.com>
2014-11-25 21:29:38 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,44 +1,97 @@
-var characters = 0;
-var money = 0;
-var bananas = 0;
-var monkeys = 0;
-var character_worth = 0.25;
-var banana_cost = 0.5;
+var ISO_ODDS = 1/5;
+var SATURATION_PT = 100;
+var requiredStreak = 10;
+var showRequiredStreak = true;
+var insideConcentration = 0;
+var outsideConcentration = 0;
+var numberCorrect = 0;
+var numberIncorrect = 0;
+var streak = 0;
+var colors = ["red", "orange", "yellow", "green", "blue", "purple", "gray"];
 
-function update_stats() {
-	document.getElementById("characters").innerHTML = characters;
-	document.getElementById("money").innerHTML = money;
-	document.getElementById("bananas").innerHTML = bananas;
+function shouldBeIsotonic() {
+	return (Math.random() <= ISO_ODDS);
 }
 
-function type_character() {
-	characters++;
-	update_stats();
+function updateEnvironment() {
+	document.getElementById("inside-solute-percent").innerHTML = insideConcentration.toString();
+	document.getElementById("inside-water-percent").innerHTML = (100 - insideConcentration).toString();
+	document.getElementById("outside-solute-percent").innerHTML = outsideConcentration.toString();
+	document.getElementById("outside-water-percent").innerHTML = (100 - outsideConcentration).toString();
+	document.getElementById("water-front").setAttribute("style", "opacity: " + ((outsideConcentration)/100).toString());
+	document.getElementById("cell-front").setAttribute("style", "opacity: " + ((insideConcentration)/100).toString());
 }
 
-function publish() {
-	var earned = Math.floor(characters * character_worth);
-	if (earned < 1) {
-		return;
+function newEnvironment() {
+	insideConcentration = Math.floor(Math.random() * (SATURATION_PT + 1));
+	if (shouldBeIsotonic()) {
+		outsideConcentration = insideConcentration;
 	}
-	characters -= Math.floor(earned) / character_worth;
-	money += earned / 100;
-	update_stats();
+	else {
+		outsideConcentration = Math.floor(Math.random() * (SATURATION_PT + 1));
+	}
+	updateEnvironment();
+	updateScores();
 }
 
-function buy_banana() {
-	if (money < banana_cost) {
-		return;
+function getSolution() {
+	if (insideConcentration < outsideConcentration) {
+		return "hypertonic";
 	}
-	money -= banana_cost;
-	bananas++;
-	update_stats();
+	else if (insideConcentration > outsideConcentration) {
+		return "hypotonic";
+	}
+	else {
+		return "isotonic";
+	}
 }
 
-function feed_monkey() {
-	if (banana < 1) {
-		return;
+function updateScores() {
+	document.getElementById("streak").innerHTML = streak.toString();
+	document.getElementById("correct").innerHTML = numberCorrect.toString();
+	document.getElementById("incorrect").innerHTML = numberIncorrect.toString();
+	document.getElementById("required-streak").innerHTML = requiredStreak.toString();
+}
+
+function changeRequiredStreak(newRequirement) {
+	requiredStreak = newRequirement;
+	updateScores();
+}
+
+function notify(correct) {
+	if (correct) {
+		document.getElementById("solution").setAttribute("class", "correct");
 	}
-	bananas--;
-	monkeys++;
+	else {
+		document.getElementById("solution").setAttribute("class", "incorrect");
+	}
+}
+
+function showSolution(solution) {
+	document.getElementById("solution").innerHTML = solution.toUpperCase();
+}
+
+function changeColor(num) {
+	document.getElementById("cell-front").setAttribute("class", "cell " + colors[num]);
+	document.getElementById("water-front").setAttribute("class", "water " + colors[num]);
+}
+
+function selectType(guess) {
+	var solution = getSolution();
+	if (guess == getSolution()) {
+		numberCorrect++;
+		streak++;
+		notify(true);
+	}
+	else {
+		numberIncorrect++;
+		streak = 0;
+		notify(false)
+	}
+	showSolution(solution);
+	newEnvironment();
+}
+
+window.onload = function () {
+	newEnvironment();
 }
